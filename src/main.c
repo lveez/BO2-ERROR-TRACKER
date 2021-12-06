bb3e42a790577ff0e02cc5f40398970ce899aaf5
lveez <barneslouis225@gmail.com>
2021-12-06 17:47:32 +0000

    fixed addresses so they compile in C

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include "memory/addresses.h"
+#include "memory/memory.h"
+#include "display/display.h"
 
 int main() {
-  printf("Hello World");
-  return 0;
+  
 }
--- src/memory/addresses.h
+++ src/memory/addresses.h
@@ -12,8 +12,8 @@ mov esi, dword ptr [ebx + 0x2]
 */
 typedef struct
 {
-    const unsigned int ptr = 0x2dea400;
-    const unsigned char next_offset = 0x2;
+    const unsigned int ptr;
+    const unsigned char next_offset;
 } ParentVar;
 
 /*
@@ -23,23 +23,35 @@ mov esi, dword ptr [esi + 0x4]
 */
 typedef struct  
 {
-    const unsigned int ptr = 0x2dea580;
-    const unsigned char next_offset = 0x4;
+    const unsigned int ptr;
+    const unsigned char next_offset;
 } ChildVar;
 
 // next free animinfo index is just stored in memory statically
 typedef struct
 {
-    const unsigned int next = 0x2b3f14a;
+    const unsigned int next;
 } AnimInfo;
 
-struct Addresses
+typedef struct
 {
     ParentVar parent_var;
     ChildVar child_var;
     AnimInfo anim_info;
+    unsigned int num_snapshot_entities;
+    unsigned int num_entities;
+} Addresses;
+
+Addresses InitAddresses()
+{
+    ParentVar parent_var = {0x2dea400, 0x2};
+    ChildVar child_var = {0x2dea580, 0x4};
+    AnimInfo anim_info = {0x2b3f14a};
     unsigned int num_snapshot_entities = 0x1278020;
     unsigned int num_entities = 0x1278010;
-};
+
+    Addresses addresses = {parent_var, child_var, anim_info, num_snapshot_entities, num_entities};
+    return addresses;
+}
 
 #endif
